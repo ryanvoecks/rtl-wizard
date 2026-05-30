6b32095ae7d7a0e722d6aec7cbaac479818be606
Ryan Voecks <voecksryan@gmail.com>
2026-05-30 20:13:34 +0000

    Fix AXI test bench

--- common/scripts/verilog_axi.sh
+++ common/scripts/verilog_axi.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Upstream cocotb regression (33 cases).
 set -eo pipefail
-PARAM_S_COUNT=8 PARAM_M_COUNT=8 make -C tb/axi_crossbar 2>&1 | tee axi_sim.log
+make -C tb/axi_crossbar PARAM_S_COUNT=8 PARAM_M_COUNT=8 PARAM_M_ID_WIDTH=11 2>&1 | tee axi_sim.log
 grep -q 'FAIL=0 SKIP=0' axi_sim.log
